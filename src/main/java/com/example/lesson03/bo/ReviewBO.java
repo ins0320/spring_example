a31f60f91dbabc044b6de0f5b18e436a3757ba38
ins0320 <ins0320@naver.com>
2023-12-19 18:43:19 +0900

    [김유정] - 3강 insert, update 문제

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -10,8 +10,28 @@ import com.example.lesson03.mapper.ReviewMapper;
 public class ReviewBO {
 	@Autowired
 	private ReviewMapper reviewMapper;
+	
 	// input: id  output: Review ( 리스트 아닌 하나만 리턴 하니까)//ReviewMapper에서 받아옴
 	public Review getReview(int id) { // null 허용 없음: int
 		return reviewMapper.selectReview(id);
+	}
+	
+	// input: Review  output: 성공한 행의 개수(int)
+	public int addReview(Review review) {
+		return	reviewMapper.insertReview(review);
+	}
+	
+	// addReviewAsFiled(4, "콤비네이션R", "바다", 5.0, "역시 맛있다!");
+	public int addReviewAsFiled(
+			int storeId, String menu,
+			String userName, Double point
+			,String review) {
+	
+		return reviewMapper.insertReviewAsFiled(storeId, menu, userName, point, review);
+	}
+	
+	// input: id, review     output: 성공한 행의 개수
+	public int updateReviewById(int id, String review) {
+		return reviewMapper.updateReviewById(id, review);
 	}
 }
--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.domain.Review;
@@ -8,4 +9,22 @@ import com.example.lesson03.domain.Review;
 public interface ReviewMapper {
 
 	public Review selectReview(int id);
+	
+	// input: Review  output: 성공한 행의 개수(int)
+	// 파라미터가 하나면 map으로 감쌀 필요 없다: @Param X!
+	public int insertReview(Review review);
+	
+	public int insertReviewAsFiled(
+			// 여러개의 파라미터를 map으로 감싸주는 @Param
+			@Param("storeId") int storeId1
+			,@Param("menu") String menu1
+			,@Param("userName") String userName
+			,@Param("point") Double point
+			,@Param("review") String review		
+			);
+	
+	// input: id, review     output: 성공한 행의 개수
+	public int updateReviewById(
+			@Param("id") int id
+			,@Param("review") String review);
 }
